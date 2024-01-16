65b4bef80b555df63e2853295ca52626b8a9b954
Daniel Moran Vilchez <skaan.dmv@gmail.com>
2024-01-16 17:36:19 -0500

    Https

--- dist/routes/imageQrWhatsapp.js
+++ dist/routes/imageQrWhatsapp.js
@@ -29,7 +29,7 @@ const handleImageNotFoundError = (req, res, next) => {
 // Ruta para generar la URL dinámica del código QR
 router.get("/urlqr/:nameimg", handleImageNotFoundError, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const dynamicURL = `${req.protocol}://${req.get("host")}/api/v1/imgqrwp/urlqr/image/${req.params.nameimg}/${Date.now()}`;
+        const dynamicURL = `${req.protocol}s://${req.get("host")}/api/v1/imgqrwp/urlqr/image/${req.params.nameimg}/${Date.now()}`;
         res.json({ url: dynamicURL });
     }
     catch (error) {
